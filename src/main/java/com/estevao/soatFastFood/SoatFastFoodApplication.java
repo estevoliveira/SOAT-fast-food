974f529278e32686e74f57a4aaec430472cefe23
Estevao <estevoliveira1@gmail.com>
2025-01-01 22:36:53 -0300

    first commit

---
+++ src/main/java/com/estevao/soatFastFood/SoatFastFoodApplication.java
@@ -0,0 +1,13 @@
+package com.estevao.soatFastFood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoatFastFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoatFastFoodApplication.class, args);
+	}
+
+}
